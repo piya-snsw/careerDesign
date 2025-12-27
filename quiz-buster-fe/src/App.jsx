34829422319c84a6e2769b6b8be23064855715ac
sukegawa-yuuya <siw24010019@class.siw.ac.jp>
2025-12-27 14:01:24 +0900

    server

--- quiz-buster-fe/src/App.jsx
+++ quiz-buster-fe/src/App.jsx
@@ -42,6 +42,7 @@ function App() {
 
   const loginWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: 'select_account' });
     await signInWithPopup(auth, provider);
   };
 
