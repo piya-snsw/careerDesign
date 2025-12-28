a886e0e916370f6a471af201191c5e9bfac37667
宮澤文香 <siw24010036@class.siw.ac.jp>
2025-12-28 17:44:37 +0900

    .envの修正

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:REACT_APP_FIREBASE_API_KEY,
-  authDomain:REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId:REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket:REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId:REACT_APP_FIREBASE_APP_ID
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_IDD,
+  appId:import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Firebaseの初期化
